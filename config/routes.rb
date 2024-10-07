e90edb675679282a8297e1c57cf641ef582c79ca
aitsuki16 <aitsuki26@gmail.com>
2024-10-07 15:14:49 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+end
